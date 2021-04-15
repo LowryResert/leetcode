e6cae9ec79390e2ce67355686509a1c9a6fd945d
ruiqingluo <754264592@qq.com>
2021-04-15 19:27:46 +0800

    #208. Implement Trie (Prefix Tree)

---
+++ src/dailyPractice/Solution208.java
@@ -0,0 +1,12 @@
+package dailyPractice;
+
+import dailyPractice.implement.Trie;
+
+/**
+ * #208. Implement Trie (Prefix Tree)
+ */
+public class Solution208 {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+    }
+}
